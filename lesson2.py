bd71b8d1c48a33f5777aecafc6b358387da734a3
MaksimBogdanov26 <createforlem102gem@mail.ru>
2024-09-10 23:04:48 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+count_HW = 12;
+count_HW_0 = "затрачено часов: 12"
+count_hour = 1.5;
+count_hour_0 = "затрачено часов: 1.5"
+name = "Python"
+average = count_hour/count_HW
+print(name + ',',count_HW_0 + ',',count_hour_0 + ',',"среднее выполнение задачи",average)
